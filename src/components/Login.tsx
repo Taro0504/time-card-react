e1eb62b239e72f3d2e5ec65269506c8f6b4bb60d
Taro0504 <nyankoiscat0504@gmail.com>
2023-08-18 13:59:37 +0900

    create react app

---
+++ src/components/Login.tsx
@@ -0,0 +1,113 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+
+type FormData = {
+  email: string;
+  password: string;
+};
+
+function Login() {
+  const { register, handleSubmit, formState } = useForm<FormData>();
+  const { errors } = formState;
+  const navigate = useNavigate();
+
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    try {
+      const response = await fetch("/api/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      const result = await response.json();
+      if (result.id) {
+        navigate("/login");
+      } else {
+        // エラーメッセージ
+      }
+    } catch (error) {
+      console.error("登録エラー:", error);
+    }
+  };
+
+  return (
+    <div className="flex h-screen bg-white">
+      <div className="w-1/2 flex bg-[url('/public/images/login-bg.png')] bg-no-repeat bg-contain bg-center">
+        <div className="ml-6 mt-6">
+          <FontAwesomeIcon icon={faCoffee} />
+          <span className="text-xl font-bold text-black">Functional Lab</span>
+        </div>
+      </div>
+      <div className="w-1/2 bg-main-2 flex flex-col items-center justify-center">
+        <h1 className="text-2xl mb-8 text-black">Welcome to Functional Lab!</h1>
+        <div className="bg-white flex flex-col items-center justify-center text-center content-center rounded-lg">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="p-8 rounded-lg shadow-md w-96"
+          >
+            <div className="mb-4 relative flex flex-col text-center">
+              <label
+                htmlFor="email"
+                className="block text-gray-300 mb-2"
+              >
+                メールアドレス
+              </label>
+              <input
+                id="email"
+                type="email"
+                {...register("email", { required: true })}
+                className="w-full p-2 border-b border-main-2 bg-transparent text-gray-700"
+                placeholder="メールアドレス"
+              />
+              {errors.email && (
+                <span className="text-red text-sm mt-1">
+                  メールアドレスを入力してください
+                </span>
+              )}
+            </div>
+            <div className="mb-4 relative flex flex-col">
+              <label
+                htmlFor="password"
+                className="block text-gray-700 mb-2"
+              >
+                パスワード
+              </label>
+              <input
+                id="password"
+                type="password"
+                {...register("password", { required: true })}
+                className="w-full p-2 border-b border-main-2 bg-transparent text-gray-700"
+                placeholder="パスワード"
+              />
+              {errors.password && (
+                <span className="text-red text-sm mt-1">
+                  パスワードを入力してください
+                </span>
+              )}
+            </div>
+            <div className="px-4 py-2">
+              <button
+                type="submit"
+                className="w-full bg-main text-gray p-2 rounded-md hover:bg-green-600"
+              >
+                ログイン
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="mt-4">
+          <p className="h-100">
+            アカウント登録がまだの方は
+            <Link to="/register" className="text-main rounded hover:text-red text-center">こちら</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/components/Register.tsx
@@ -0,0 +1,84 @@
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+
+type FormData = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+function Register() {
+  const { register, handleSubmit, formState } = useForm<FormData>();
+  const { errors } = formState;
+  const navigate = useNavigate();
+
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    // ...
+  };
+
+  return (
+    <div className="flex h-screen">
+      <div className="w-1/2 flex bg-[url('/public/images/register-bg.png')] bg-no-repeat bg-contain bg-center">
+        {<div className="w-1/2 flex bg-[url('/public/images/login-bg.png')] bg-no-repeat bg-contain bg-center">
+        <div className="ml-6 mt-6">
+          <FontAwesomeIcon icon={faCoffee} />
+          <span className="text-xl font-bold text-black">Functional Lab</span>
+        </div>
+      </div>
+      </div>
+      <div className="w-1/2 bg-main-2 flex items-center justify-center">
+        <div className="bg-white flex flex-col items-center justify-center text-center content-center rounded-lg">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="p-8 rounded-lg shadow-md w-96"
+          >
+            <div className="mb-4 relative flex flex-col text-center">
+              <label className="block text-gray-300 mb-2">名前</label>
+              <input
+                {...register('name', { required: true })}
+                className="w-full p-2 border-b border-blue-500 bg-transparent text-gray-700"
+                placeholder="名前"
+              />
+              {errors.name && (
+                <span className="text-red text-sm mt-1">このフィールドは必須です</span>
+              )}
+            </div>
+            <div className="mb-4 relative flex flex-col text-center">
+              <label className="block text-gray-300 mb-2">メールアドレス</label>
+              <input
+                {...register('email', { required: true })}
+                className="w-full p-2 border-b border-blue-500 bg-transparent text-gray-700"
+                placeholder="メールアドレス"
+              />
+              {errors.email && (
+                <span className="text-red text-sm mt-1">このフィールドは必須です</span>
+              )}
+            </div>
+            <div className="mb-4 relative flex flex-col text-center">
+              <label className="block text-gray-700 mb-2">パスワード</label>
+              <input
+                type="password"
+                {...register('password', { required: true })}
+                className="w-full p-2 border-b border-green-500 bg-transparent text-gray-700"
+                placeholder="パスワード"
+              />
+              {errors.password && (
+                <span className="text-red text-sm mt-1">このフィールドは必須です</span>
+              )}
+            </div>
+            <div className="px-4 py-2">
+              <button
+                type="submit"
+                className="w-full bg-main text-gray p-2 rounded-md hover:bg-green-600"
+              >
+                新規登録
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
